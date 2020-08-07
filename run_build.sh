b4f8401d1a848261e148963a1e2e7a641b4e529f
sandeepkumar-skb <sandeep.skb@uber.com>
2020-08-06 19:38:52 -0700

    updating cmakelist

--- run_build.sh
+++ run_build.sh
@@ -4,6 +4,6 @@ rm -rf build;
 mkdir build;
 cd build
 
-cmake -DCMAKE_PREFIX_PATH=/home/sandeep/anaconda3/lib/python3.7/site-packages/torch ..
+cmake -DCMAKE_PREFIX_PATH="$(python -c 'import torch; print(torch.__path__[0])')" ..
 
 make -j$(nproc)
